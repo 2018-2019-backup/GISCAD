4ff908274a0805da366d1e6edaade7632b3ada25
KKMSoft-SourceCode <52309871+KKMSoft-SourceCode@users.noreply.github.com>
2019-09-11 11:48:24 +0530

    GISCAD

---
+++ Giscad2018-V2/Inernal_V2/AssetMgr/stdafx.h
@@ -0,0 +1,28 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+// TODO: reference additional headers your program requires here
+#ifndef WINVER					//----- Allow use of features specific to Windows 95 and Windows NT 4 or later.
+#define WINVER 0x0501			//----- Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
+#endif
+
+#ifndef _WIN32_WINNT			//----- Allow use of features specific to Windows NT 4 or later.
+//#define _WIN32_WINNT 0x0400		//----- Change this to the appropriate value to target Windows 2000 or later.
+#define _WIN32_WINNT 0x0501
+#endif						
+
+#ifndef _WIN32_WINDOWS			//----- Allow use of features specific to Windows 98 or later.
+#define _WIN32_WINDOWS 0x0501	//----- Change this to the appropriate value to target Windows Me or later.
+#endif
+
+#ifndef _WIN32_IE				//----- Allow use of features specific to IE 4.0 or later.
+#define _WIN32_IE 0x0501		//----- Change this to the appropriate value to target IE 5.0 or later.
+#endif
