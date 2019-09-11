0a0546d426e35e2cb6c8f5dfd89aed420f4a6806
KKMSoft-SourceCode <52309871+KKMSoft-SourceCode@users.noreply.github.com>
2019-09-11 11:45:58 +0530

    GISCAD

---
+++ Giscad2018-V2/Inernal_V2/AcadValidation/resource.h
@@ -0,0 +1,45 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by AcadValidation.rc
+//
+#define IDS_PROJNAME                    100
+#define IDR_HTML_NSVALIDATORDLG         103
+#define IDD_VALIDATE_DIALOG             201
+#define IDD_ELLIPSE_DIALOG              202
+#define IDD_REPORT_DIALOG               203
+#define IDC_BUTTON_HELP                 204
+#define IDC_PROGRESS1                   205
+#define IDC_TAB1                        206
+#define IDC_CHECK1                      207
+#define IDC_EDIT_HOST                   208
+#define IDC_CHK_CADVALIDATION           208
+#define IDC_EDIT_CONN                   209
+#define IDC_CHK_GISVALIDATION           209
+#define IDC_EDIT_USER                   210
+#define IDC_EDIT_PWD                    211
+#define IDC_EDIT_DIST                   212
+#define IDC_HTML_LINK                   216
+#define IDC_EDIT1                       218
+#define IDC_REPORT_EDIT                 218
+#define IDC_EDIT1_POSITION              218
+#define IDC_STATIC_POSITION             219
+#define IDC_COMBO1                      220
+#define IDC_CHK_P1_ERRORS               221
+#define IDC_CHK_P2_ERRORS               222
+#define IDC_CHK_P3_ERRORS               223
+#define IDC_CHECK5                      224
+#define IDC_CHK_P4_ERRORS               224
+#define IDC_VALIDATION_ERRORS           225
+#define IDR_MINIFRAME                   30994
+#define IDR_MAINFRAME                   30994
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        208
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         226
+#define _APS_NEXT_SYMED_VALUE           104
+#endif
+#endif
